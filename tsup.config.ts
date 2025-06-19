15b8b46d527e88f17031f5339a138e7739584296
munseunggyu <femun0427@sonuvis.ai>
2025-06-19 16:09:15 +0900

    electron 관련 의존성 추가

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./electron/main.ts", "./electron/preload.ts"],
+  entry: ["./electron/src/main.ts", "./electron/src/preload.ts"],
   splitting: false,
   sourcemap: false,
   clean: true,
